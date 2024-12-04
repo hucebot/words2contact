70bec3ccfd3e9ed1d46a3e9c7d00b649d21d62f1
dtotsila <dionisistotsilas@gmail.com>
2024-12-04 15:51:32 +0100

    fix launch, rename llm_utils

--- launch.sh
+++ launch.sh
@@ -6,8 +6,8 @@ docker run --rm \
     -v $(pwd)/words2contact/prompts/:/home/words2contact/prompts/ \
     -v $(pwd)/words2contact/grammar/:/home/words2contact/grammar/ \
     -v $(pwd)/words2contact/scripts/:/home/words2contact/scripts/ \
-    -v $(pwd)/utils/config/:/home/words2contact/config/ \
-    -v $(pwd)/utils/CLIP_Surgery/:/home/words2contact/scripts/CLIP_Surgery \
+    -v $(pwd)/config/:/home/words2contact/config/ \
+    -v $(pwd)/submodules/CLIP_Surgery/:/home/words2contact/scripts/CLIP_Surgery \
     -v $(pwd)/data/:/home/words2contact/data/ \
     --ipc host \
     --device /dev/bus/usb \
